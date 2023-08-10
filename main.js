82fab66057b09e8536df7c8d7b1ff5bed678daab
solooday6 <141855464+solooday6@users.noreply.github.com>
2023-08-10 14:32:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let nama = 'Inna al bashir'
+let pesanUntuk = 'Nadiya Nahdah'
+let usia = 21
+alert('Halo, ini ' + nama + ', pesan ini untuk ' + pesanUntuk + ' tulisan sederhana untuk sahabat juga kakak saya yang sekarang sudah berusia ' + usia + ' tahun yang juga lagi mau KM dan berangkatnya hari ini (hati-hati yaa) ' + '. Dan semoga pesan ini bisa memberi kebahagiaan, Aamiin ')
